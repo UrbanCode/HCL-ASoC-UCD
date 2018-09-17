aba21cae339523b0949eede60e3e35205073d24b
Jeffrey Barto <bartorobjeff@gmail.com>
2018-09-17 18:17:55 -0400

    Fixed build process to download dependencies and align with current plugin structure.

---
+++ src/main/zip/delete_presence.groovy
@@ -0,0 +1,21 @@
+/**
+ * � Copyright IBM Corporation 2015.
+ * This is licensed under the following license.
+ * The Eclipse Public 1.0 License (http://www.eclipse.org/legal/epl-v10.html)
+ * U.S. Government Users Restricted Rights:  Use, duplication or disclosure restricted by GSA ADP Schedule Contract with IBM Corp.
+ */
+
+import com.urbancode.air.AirPluginTool
+import com.urbancode.air.plugin.AppScanSaaS.SCXRestClient
+
+final def airHelper = new AirPluginTool(args[0], args[1])
+final Properties props = airHelper.getStepProperties()
+boolean deleteAll = Boolean.parseBoolean(props['deleteAll'])
+String presenceId = props['presenceId']
+
+SCXRestClient restClient = new SCXRestClient(props)
+restClient.deletePresence(presenceId, deleteAll)
+
+airHelper.setOutputProperty("PresenceId", presenceId)
+airHelper.storeOutputProperties()
+println "[OK] Process has completed successfully."
---
+++ src/main/zip/start_dast_scan.groovy
@@ -0,0 +1,65 @@
+/**
+ * � Copyright IBM Corporation 2015.
+ * This is licensed under the following license.
+ * The Eclipse Public 1.0 License (http://www.eclipse.org/legal/epl-v10.html)
+ * U.S. Government Users Restricted Rights:  Use, duplication or disclosure restricted by GSA ADP Schedule Contract with IBM Corp.
+ */
+
+import com.urbancode.air.AirPluginTool
+import com.urbancode.air.plugin.AppScanSaaS.SCXRestClient
+import com.urbancode.air.plugin.AppScanSaaS.ScanType
+
+final def airHelper = new AirPluginTool(args[0], args[1])
+final Properties props = airHelper.getStepProperties()
+
+String appId = props["applicationId"]
+String startingUrl = props["startingUrl"]
+String issueCountString = props['reportIssueCountValidation'] // Fail count threshold
+String scanUser = props["scanUser"]
+String scanPassword = props["scanPassword"]
+String thirdCredential = props['thirdCredential']
+String parentjobid = props["parentScanId"]
+String scanType = props["scanType"]
+String scanFilePath = props["scanFile"]
+String presenceId = props["presenceId"]
+String testPolicy = props["testPolicy"]
+long scanTimeout = props["scanTimeout"] ? Long.parseLong(props["scanTimeout"]) : -1
+boolean failOnPause = Boolean.parseBoolean(props['failOnPause'])
+boolean validateReport = !issueCountString.isEmpty()   // Do not validate report if no fail condition
+int exitCode = 0
+
+SCXRestClient restClient = new SCXRestClient(props)
+
+if (startingUrl == null || startingUrl.isEmpty()){
+    println "[Error] Missing starting url."
+    System.exit(1)
+}
+
+String scanId = restClient.startDastScan(
+    startingUrl,
+    scanUser,
+    scanPassword,
+    thirdCredential,
+    parentjobid,
+    presenceId,
+    testPolicy,
+    appId,
+    scanType,
+    scanFilePath)
+
+airHelper.setOutputProperty("ScanId", scanId)
+airHelper.storeOutputProperties()
+
+if (validateReport) {
+    long startTime = System.currentTimeMillis()
+    exitCode = restClient.waitForScan(scanId, ScanType.DAST, issueCountString, startTime, scanTimeout, failOnPause)
+}
+
+if (exitCode) {
+    println("[Error] Scan has failed validation.")
+}
+else {
+    println("[OK] Scan has completed successfully.")
+}
+
+System.exit(exitCode)
