17c9b3c6f660eb30fed96225d5bda7e1fcb35b37
Ajeet Kumar <ajeet-k@hcl.com>
2025-01-22 10:31:33 +0530

    modified API payload

--- src/main/groovy/com/urbancode/air/plugin/AppScanSaaS/RestClient.groovy
+++ src/main/groovy/com/urbancode/air/plugin/AppScanSaaS/RestClient.groovy
@@ -43,7 +43,7 @@ public abstract class RestClient {
 
     private static final String API_V2 = "/api/V4/%s"
     private static final String API_BLUEMIX_LOGIN = "/api/V4/Account/BluemixLogin"
-    private static final String API_APIKEY_LOGIN = "/api/V4/Account/ApiKeyLogin"
+    private static final String API_APIKEY_LOGIN = "/api/v4/Account/ApiKeyLogin"
     private static final String REPORT_TYPE_XML = "Xml"
     private static final String REPORT_TYPE_PDF = "Pdf"
     private static final String REPORT_TYPE_HTML = "Html"
@@ -52,7 +52,7 @@ public abstract class RestClient {
     private static final String API_FILE_UPLOAD = "/api/V4/FileUpload"
     private static final String MOBILE_API_PATH = "/api/V4/%s/MobileAnalyzer"
     private static final String SAST_API_PATH = "/api/V4/%s/StaticAnalyzer"
-    private static final String DAST_API_PATH = "/api/V4/%s/Dast"
+    private static final String DAST_API_PATH = "/api/v4/%s/Dast"
     protected static final String DAST_FILE_API_PATH = "/api/V4/%s/DynamicAnalyzerWithFile"
     private static final String API_METHOD_SCANS = "Scans"
     private static final String API_RE_SCAN = "/api/V4/Scans/%s/Executions"
@@ -463,9 +463,37 @@ public abstract class RestClient {
             }
 
             /* Empty fields are ignored */
-            props.putAll([ AppId: appId, ScanName : scanName, StartingUrl : startingUrl, LoginUser : loginUsername,
-                LoginPassword : loginPassword, ExtraField: thirdCredential, PresenceId : presenceId,
-                testPolicy : testPolicyForPostRequest, ScanType: scanType, EnableMailNotification: mailNotification])
+            props.putAll([
+                ScanName : scanName,
+                EnableMailNotification: mailNotification,
+                Locale : "string",
+                AppId: appId,
+                Execute : true,
+                FullyAutomatic : true,
+                Personal : true,
+                Comment : "Test",
+                ScanConfiguration : [
+                    Target              : [
+                        StartingUrl                  : startingUrl,
+                        ShouldScanBelowThisDirectory: true,
+                        UseCaseSensitivePaths       : true
+                    ],
+                    Tests               : [
+                        PredefinedTestPolicy        : "Complete",
+                        TestOptimizationLevel       : "NoOptimization",
+                        TestLoginPages              : true,
+                        TestLoginPagesWithoutSessionIds: true,
+                        TestLogoutPages             : true,
+                        DetectVulnerableComponents  : true
+                    ],
+                    ApplicationElements : [
+                        EnableAutomaticFormFill     : true
+                    ]
+                ],
+                IncludeVerifiedDomains  : true,
+                OnlyFullResults         : true,
+                TestOperation           : "None"
+            ])
         }
 
         props.put("ClientType", clientType) // Configure clientType for both new scans and re-scans
